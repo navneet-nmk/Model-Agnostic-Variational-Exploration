b8259e37ab11fb49fb6a43681b9635855fa28617
Riya <riyasharma031995@gmail.com>
2019-01-16 11:09:26 +0100

    Appended python path

--- maesn/launchers/train.py
+++ maesn/launchers/train.py
@@ -1,44 +1,53 @@
-from sandbox.rocky.tf.algos.maesn_trpo import MAESN_TRPO
+import sys
+sys.path.append("/Users/navneetmkumar/Downloads/maesn_suite/maesn/")
+print(sys.path)
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from rllab.baselines.zero_baseline import ZeroBaseline
 from rllab.envs.normalized_env import normalize
 from rllab.misc.instrument import stub, run_experiment_lite
+import sandbox
 from sandbox.rocky.tf.policies.maml_minimal_gauss_mlp_policy_adaStep import MAMLGaussianMLPPolicy as adaGaussPolicy
 from sandbox.rocky.tf.policies.maml_minimal_gauss_mlp_policy import MAMLGaussianMLPPolicy as GaussPolicy
 from sandbox.rocky.tf.envs.base import TfEnv
+from sandbox.rocky.tf.algos.maesn_trpo import MAESN_TRPO
 import argparse
 import tensorflow as tf
-from hyperparam_sweep import VG
+from launchers.hyperparam_sweep import VG
 from rllab.envs.mujoco.wheeled_robot import WheeledEnv
 from rllab.envs.mujoco.pusher import PusherEnv
 from rllab.envs.mujoco.ant_env_rand_goal_ring import AntEnvRandGoalRing
 
-mode = 'local_docker'
+mode = 'local'
 #mode = 'ec2'
-parser = argparse.ArgumentParser()
-parser.add_argument('algo' , type=str , help = 'Maesn or LSBaseline')
-parser.add_argument('--env', type=str,
-                    help='currently supported envs are Pusher, Wheeled and Ant')
-args = parser.parse_args()
-assert args.algo in ['Maesn' , 'LSBaseline']
-assert args.env in ['Ant' , 'Pusher', 'Wheeled']
+#parser = argparse.ArgumentParser()
+#parser.add_argument('algo' , type=str , help = 'Maesn or LSBaseline', default='Maesn')
+#parser.add_argument('--env', type=str,
+#                    help='currently supported envs are Pusher, Wheeled and Ant', default='Pusher')
+#args = parser.parse_args()
+#assert args.algo in ['Maesn' , 'LSBaseline']
+#assert args.env in ['Ant' , 'Pusher', 'Wheeled']
 
 variants = VG().variants()
 num_total_tasks = 100 ; num_grad_updates = 1 ; n_itr = 500
-for v in variants:  
+
+aalgo = 'Maesn'
+eenv = 'Pusher'
+
+for v in variants:
+
     
-    stub(globals())
+    #stub(globals())
     ####################Env Selection#####################
-    if args.env == 'Pusher':
-        env = TfEnv( normalize(PusherEnv()))
+    if eenv == 'Pusher':
+        env = TfEnv(normalize(PusherEnv()))
         max_path_length = 100
 
-    elif args.env == 'Wheeled':
-        env = TfEnv( normalize(WheeledEnv()))
+    elif eenv == 'Wheeled':
+        env = TfEnv(normalize(WheeledEnv()))
         max_path_length = 200
 
-    elif args.env == 'Ant':
-        env = TfEnv( normalize(AntEnvRandGoalRing()))
+    elif eenv == 'Ant':
+        env = TfEnv(normalize(AntEnvRandGoalRing()))
         max_path_length = 200
 
     else:
@@ -46,7 +55,7 @@ for v in variants:
     ########################################################
 
     #####################Algo Selection####################
-    if args.algo == 'Maesn':
+    if aalgo == 'Maesn':
         assert v['fast_learning_rate'] != 0 , 'Fast learning rate needs to be non 0 for Maesn'
         policy = adaGaussPolicy(
             name="policy",
@@ -58,7 +67,7 @@ for v in variants:
             num_total_tasks=num_total_tasks,
             init_std = v['init_std'],
         )
-    elif args.algo == 'LSBaseline':
+    elif aalgo == 'LSBaseline':
         assert v['fast_learning_rate'] == 0 ,'Fast learning rate needs to be 0 for LS Baseline'
         policy =  GaussPolicy(
             name="policy",
@@ -74,8 +83,8 @@ for v in variants:
         raise AssertionError('Not Implemented')
     ########################################################
 
-
     baseline = LinearFeatureBaseline(env_spec=env.spec)
+    print(env)
 
     algo = MAESN_TRPO(
         env=env,
@@ -95,16 +104,19 @@ for v in variants:
         #plottingFolder = "Sparse_BP_kl0.05_ldim2",
         kl_scheme=None
     )
-    run_experiment_lite(
-        algo.train(),
-        n_parallel=1,
-        snapshot_mode="all",
-        #python_command='python3',
-        seed=v['seed'],
-        exp_prefix=args.algo+'_'+args.env,
-        exp_name= v['exp_name'],
-        plot=False,
-        sync_s3_pkl=True,
-        mode=mode,
-        
-    )
+
+    algo.train()
+
+    #run_experiment_lite(
+    #    algo.train(),
+    #    n_parallel=1,
+    #    snapshot_mode="all",
+    #    #python_command='python3',
+    #    log_dir='/Users/navneetmkumar/Downloads/maesn_suite/maesn/tmp',
+    #    seed=v['seed'],
+    #    exp_prefix=aalgo+'_'+eenv,
+    #    exp_name= v['exp_name'],
+    #    plot=False,
+    #    sync_s3_pkl=True,
+    #    mode=mode,
+    #)
